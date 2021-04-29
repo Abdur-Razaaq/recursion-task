d4faf3945fb833db1cafeba5bc1d0166b9432ad3
Abdur-Razaaq Jardien <zakjardien23@gmail.com>
2021-04-29 11:35:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# Python program to display the Fibonacci sequence
 def fibonacci(number):
+    # ensuring that only positive numbers are accpeted
     if number <= 1:
         return number
+    # the formula that calculates the fibonacci sequence
     else:
         return fibonacci(number-1) + fibonacci(number-2)
 
-
+# the number of terms of the fibonacci sequence that are wanted
 nth_term = 20
 
 print("Fibonacci sequence: ")
+# setting a range from the first term to the nth_term 
 for i in range(nth_term):
+    # printing the answers. The "end" parameter appends space 
+    # instead of allowing each term to start on a new line.
+    # The space between the inverted commas signify
+    # that there should be spaces between each term.
     print(fibonacci(i), end=" ")
-
-"""print(fibonacci(0), fibonacci(1), fibonacci(2), fibonacci(3), fibonacci(4), fibonacci(5), fibonacci(6), fibonacci(7),
-      fibonacci(8), fibonacci(9), fibonacci(10), fibonacci(11), fibonacci(12), fibonacci(13), fibonacci(14),
-      fibonacci(15), fibonacci(16), fibonacci(17), fibonacci(18), fibonacci(19))"""
